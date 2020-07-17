630310572362382eca7d54619837f88c5cf97f81
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-07-17 12:32:08 +0100

    setinggan awal

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+require('dotenv').config();
+
+const authRoute = require('./routes/Auth.route')
+
+const app = express();
+app.use(morgan('dev'))
+
+app.get('/', async (req, res, next) => {
+    res.send("Hallo from express")
+})
+
+app.use('/auth', authRoute);
+
+app.use(async (req, res, next) => {
+    next(createError.NotFound())
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server runing on port ${PORT}`)
+})
